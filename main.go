4f0eee07ec4bfd7a1a94b811652712dcad2e874a
joeoakes <jxo19@psu.edu>
2023-10-22 11:07:10 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
+
+// Block represents each 'item' in the blockchain
+type Block struct {
+	Index     int
+	Timestamp string
+	Data      string
+	PrevHash  string
+	Hash      string
+}
+
+// calculateHash creates a SHA256 hash of the block
+func (b *Block) calculateHash() string {
+	record := fmt.Sprintf("%d%s%s%s", b.Index, b.Timestamp, b.Data, b.PrevHash)
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return fmt.Sprintf("%x", hashed)
+}
+
+// createBlock is a function to create a new block
+func createBlock(index int, data string, prevHash string) *Block {
+	block := &Block{
+		Index:     index,
+		Timestamp: time.Now().String(),
+		Data:      data,
+		PrevHash:  prevHash,
+	}
+	block.Hash = block.calculateHash()
+	return block
+}
+
+func main() {
+	// Creating a new block
+	genesisBlock := createBlock(0, "Genesis Block", "")
+	fmt.Println("Genesis Block:", genesisBlock)
+}
